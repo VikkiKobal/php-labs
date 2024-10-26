6473e049f8d396bd99ab2d3cdd7203ef5950ea57
Viktoriia Kobal <161163769+VikkiKobal@users.noreply.github.com>
2024-10-26 17:16:34 +0300

    Create index.php

---
+++ lab 2/index.php
@@ -0,0 +1,7 @@
+<?php
+include_once 'enterprise_module.php';
+include_once 'enterprises.php';
+include_once 'filterFunction.php';
+include_once 'filterAction.php';
+include_once 'template.phtml';
+?>
